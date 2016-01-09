c3e679a69558822b3f92a739688ba906d08f3715
Efrén Fuentes <efrenfuentes@gmail.com>
2016-01-08 21:03:33 -0430

    Update rspec expectations

--- spec/namespace_helper_spec.rb
+++ spec/namespace_helper_spec.rb
@@ -1,12 +1,12 @@
 #
 #   Copyright 2013 PayPal Inc.
-# 
+#
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
-# 
+#
 #       http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,19 +18,19 @@ require 'spec_helper'
 
 describe 'Genio::Util::NamespaceHelper' do
   before :all do
-    @helper = Class.new do 
+    @helper = Class.new do
       include Genio::Util::NamespaceHelper
     end.new
   end
-  
+
   it "should validate namepsace is urn or uri" do
-    @helper.is_urn_ns("https://api.paypal.com/payments").should be_false
-    @helper.is_urn_ns("urn:ebay:apis:eBLBaseComponents").should be_true
+    @helper.is_urn_ns("https://api.paypal.com/payments").should be false
+    @helper.is_urn_ns("urn:ebay:apis:eBLBaseComponents").should be true
   end
-  
+
   it "should convert namespaces to packagename" do
     @helper.convert_ns_to_package("com.paypal.api").should eql "com.paypal.api"
   end
-  
-  
+
+
 end
